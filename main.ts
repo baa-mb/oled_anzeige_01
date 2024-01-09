8597a15f2a8733c471dfe81df231b471d74927de
baa <34665120+baa-mb@users.noreply.github.com>
2024-01-09 17:51:37 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    pins.servoWritePin(AnalogPin.P2, 180)
+})
+input.onButtonPressed(Button.B, function () {
+    pins.servoWritePin(AnalogPin.P2, 0)
+})
 let i = 0
+basic.showIcon(IconNames.Yes)
 OLED12864_I2C.init(60)
 OLED12864_I2C.invert(false)
 OLED12864_I2C.zoom(true)
@@ -10,5 +17,22 @@ basic.forever(function () {
     1
     )
     basic.pause(1000)
+    OLED12864_I2C.rect(
+    0,
+    0,
+    60,
+    30,
+    1
+    )
     i += 1
+    OLED12864_I2C.rect(
+    32,
+    32,
+    40,
+    20,
+    1
+    )
+    if (i % 12 == 0) {
+        OLED12864_I2C.clear()
+    }
 })
