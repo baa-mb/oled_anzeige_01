df718eef2dbf69b577c74ef82a08cedfd720f0ef
baa <34665120+baa-mb@users.noreply.github.com>
2024-01-09 18:51:37 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    pins.servoWritePin(AnalogPin.P2, 180)
+    pins.servoWritePin(AnalogPin.P1, 180)
 })
 input.onButtonPressed(Button.B, function () {
-    pins.servoWritePin(AnalogPin.P2, 0)
+    pins.servoWritePin(AnalogPin.P1, 0)
 })
 let i = 0
 basic.showIcon(IconNames.Yes)
